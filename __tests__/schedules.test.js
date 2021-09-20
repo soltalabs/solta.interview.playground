61335403e7bf30fe97b7dc0cdecb2c088eceb6cd
Shivendra Odean <shivendra.odean@gmail.com>
2021-09-21 11:57:10 +1200

    setup: add unit testing framework and structure

---
+++ __tests__/schedules.test.js
@@ -0,0 +1,9 @@
+describe('Bus schedule tests', () => {
+  test('test one', () => {
+    console.log('hello from test 1!')
+
+    const expected = true    
+
+    expect(expected).toBeTruthy()
+  })
+})
