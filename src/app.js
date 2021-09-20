7fb686d4bf0f7e58b7b70a059a1c0902b1f5db7f
Shivendra Odean <shivendra.odean@gmail.com>
2021-09-21 11:52:01 +1200

    setup: add auto reload via nodemon

--- src/app.js
+++ src/app.js
@@ -7,6 +7,4 @@ app.get('/', (req, res) => {
   res.send('Hello Solta interview!!!')
 })
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
