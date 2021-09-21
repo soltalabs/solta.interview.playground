d6d73dc1865ad4b0128db01d4971d6d3399cd7e8
Shivendra Odean <shivendra.odean@gmail.com>
2021-09-21 14:55:01 +1200

    setup: add dummy data store

---
+++ datastore/db.js
@@ -0,0 +1,17 @@
+export const schedules = [
+  {
+    busStopId: 'a16dd92a-a172-4f26-ad62-e93e4dfa724e',
+    busId: '5aac588a-d3f4-4bce-b90d-f635c3288a35',
+    times: [ '10:00:00', '11:00:00', '12:00:00' ]
+  },
+  {
+    busStopId: 'a16dd92a-a172-4f26-ad62-e93e4dfa724e',
+    busId: '302b6859-c4aa-4f28-8ea2-44076a5b09b4',
+    times: [ '13:00:00', '14:00:00', '15:00:00' ]
+  },
+  {
+    busStopId: '70ccd02f-cae1-4f5f-9052-1c0f9211cfbc',
+    busId: 'be260f75-aab4-4288-a1ff-332fe2e88075',
+    times: [ '06:00:00', '07:00:00', '08:00:00' ]
+  }
+]
